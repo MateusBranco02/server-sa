930a0e0626b62c2032571097760be15f331ce7da
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-12-05 01:14:12 -0300

    Criando arquivo .env

--- api/server.js
+++ api/server.js
@@ -6,12 +6,16 @@ import router from '../routes/routes.js';
 const port = process.env.PORT || 3000;
 const app = express();
 
-try {
-    await conexao.sync();
-    console.log('Banco de Dados sincronizado!');
-} catch (error) {
-    console.error('Erro ao sincronizar banco de dados:', error);
+const iniciar = async () => {
+    try {
+        await conexao.sync();
+        console.log('Banco de Dados sincronizado!');
+    } catch (error) {
+        console.error('Erro ao sincronizar banco de dados:', error);
+    }
 }
+
+iniciar();
 
 app.use(cors());
 app.use(express.json());
--- database.js
+++ database.js
@@ -1,7 +1,17 @@
 import { Sequelize } from 'sequelize';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const conexao = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Aceita conexões com certificados SSL não verificados
+        },
+    },
     logging: false,
 });
 
