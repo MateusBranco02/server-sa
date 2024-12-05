fecd8222502a68f5bfd976e4994960d4af2938a6
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-12-05 01:31:54 -0300

    ..

--- api/server.js
+++ api/server.js
@@ -3,19 +3,14 @@ import conexao from '../database.js';
 import cors from 'cors';
 import router from '../routes/routes.js';
 
-const port = process.env.PORT || 3000;
 const app = express();
 
-const iniciar = async () => {
-    try {
-        await conexao.sync();
-        console.log('Banco de Dados sincronizado!');
-    } catch (error) {
-        console.error('Erro ao sincronizar banco de dados:', error);
-    }
+try {
+    await conexao.sync();
+    console.log('Banco de Dados sincronizado!');
+} catch (error) {
+    console.error('Erro ao sincronizar banco de dados:', error);
 }
-
-iniciar();
 
 app.use(cors());
 app.use(express.json());
